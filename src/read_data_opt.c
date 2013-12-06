70b16a09dbf05e04706d4688c5024f226d5d313b
luizguilhermecm <luizgui@gmail.com>
2013-12-05 22:37:14 -0200

    final

---
+++ src/read_data_opt.c
@@ -0,0 +1,10 @@
+if(wi->wav_header->num_channels == 1){
+    while(it < wi->wav_header->subchunk2_size){
+        // corpo se false
+    }
+}
+else if(wi->wav_header->num_channels == 2){
+    while(it < wi->wav_header->subchunk2_size){
+        // corpo se true
+    }
+}
